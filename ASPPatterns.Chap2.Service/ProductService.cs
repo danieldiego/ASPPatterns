7357a11b08b85ffd128d5e43010dd01842c08506
daniel <daniel.diego@outlook.com>
2014-01-05 17:55:45 -0600

    Refactoring to the adapter Pattern

--- ASPPatterns.Chap2.Service/ProductService.cs
+++ ASPPatterns.Chap2.Service/ProductService.cs
@@ -6,20 +6,22 @@ namespace ASPPatterns.Chap2.Service
     public class ProductService
     {
         private IProductRepository _productRepository;
-        public ProductService(IProductRepository productRepository)
+        private ICacheStorage _cacheStorage;
+        public ProductService(IProductRepository productRepository, ICacheStorage cacheStorage)
         {
             _productRepository = productRepository;
+            _cacheStorage = cacheStorage;
         }
         public IList<Product> GetAllProductsIn(int categoryId)
         {
             IList<Product> products;
             string storageKey = string.Format("products_in_category_id_{0}", categoryId);
-            products = (List<Product>)HttpContext.Current.Cache.Get(storageKey);
+            products = _cacheStorage.Retrieve<List<Product>>(storageKey);
 
             if (products == null)
             {
                 products = _productRepository.GetAllProductsIn(categoryId);
-                HttpContext.Current.Cache.Insert(storageKey, products);
+                _cacheStorage.Store(storageKey, products);
             }
 
             return products;
