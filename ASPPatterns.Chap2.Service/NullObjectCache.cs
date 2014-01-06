78e3415b3f5a8aba539b2945c19b8ee4645ed0b6
daniel <daniel.diego@outlook.com>
2014-01-05 18:09:59 -0600

    NullObject Pattern

---
+++ ASPPatterns.Chap2.Service/NullObjectCache.cs
@@ -0,0 +1,20 @@
+﻿namespace ASPPatterns.Chap2.Service
+{
+    public class NullObjectCache : ICacheStorage
+    {
+        public void Remove(string key)
+        {
+            //Do nothing
+        }
+
+        public void Store(string key, object data)
+        {
+            //Do nothing
+        }
+
+        public T Retrieve<T>(string key)
+        {
+            return default(T);
+        }
+    }
+}
